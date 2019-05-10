467289aad1ac9cc07cc59173094e9a0cae092fd9
Chris Lincoln <chris.lincoln12@gmail.com>
2019-05-10 16:24:01 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,12 +1,4 @@
 def turn_count(board)
   
-  board = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
-  counter = 0
-  
-  
-  board.each do |turn|
-    puts "#{turn}"
-    counter += 1
-  end
 
 end
